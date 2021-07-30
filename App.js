2c47f4e6398645faef4229b89aaa3dcb87ce8e71
Biswajit Sahu <54319031+Biswajit-sahu@users.noreply.github.com>
2021-07-30 08:47:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+const birthDate = document.getElementById("birthDate");
+const luckyNumber = document.getElementById("luckyNumber");
+const output = document.getElementById("output");
+var image = document.getElementById("image");
+const form = document.getElementById("form");
+const close = document.querySelector(".close");
+const privacy = document.querySelector(".privacy");
+
+close.addEventListener("click", (e) => {
+    privacy.style.display = "none";
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var sum = 0;
+    const birthValue = birthDate.value;
+    const luckyValue = luckyNumber.value;
+
+    const birth = birthValue.split("-").join("");
+    for (const i in birth) {
+        sum = sum + birth[i];
+    }
+    if (sum % luckyValue === 0) {
+        output.textContent = "hooray ! you are a Lucky Person ";
+        image.src = "./gifs/Lucky.gif "
+    } else {
+        output.textContent = "Oops!!Your birthday is not a lucky number!";
+        image.src = "./gifs/Unlucky.gif";
+    }
+});
